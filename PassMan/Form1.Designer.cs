258a5fc3665bdb59b7c513aed5c6593a7401b852
Jimena Cilia <jimenapaola422@gmail.com>
2024-11-27 16:08:34 -0600

    Agregar archivos de proyecto.

---
+++ PassMan/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace PassMan
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.emailValue = new System.Windows.Forms.TextBox();
+            this.createUser = new System.Windows.Forms.Button();
+            this.login = new System.Windows.Forms.Button();
+            this.passValue = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(117)))), ((int)(((byte)(214)))));
+            this.label1.Location = new System.Drawing.Point(42, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(222, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Iniciar Sesion";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(117)))), ((int)(((byte)(214)))));
+            this.panel1.Location = new System.Drawing.Point(23, 205);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(241, 2);
+            this.panel1.TabIndex = 1;
+            // 
+            // emailValue
+            // 
+            this.emailValue.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.emailValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.emailValue.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(117)))), ((int)(((byte)(214)))));
+            this.emailValue.Location = new System.Drawing.Point(23, 175);
+            this.emailValue.Multiline = true;
+            this.emailValue.Name = "emailValue";
+            this.emailValue.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.emailValue.Size = new System.Drawing.Size(241, 24);
+            this.emailValue.TabIndex = 2;
+            // 
+            // createUser
+            // 
+            this.createUser.BackColor = System.Drawing.Color.Transparent;
+            this.createUser.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.createUser.ForeColor = System.Drawing.Color.Black;
+            this.createUser.Location = new System.Drawing.Point(49, 359);
+            this.createUser.Name = "createUser";
+            this.createUser.Size = new System.Drawing.Size(175, 23);
+            this.createUser.TabIndex = 3;
+            this.createUser.Text = "Crear";
+            this.createUser.UseVisualStyleBackColor = false;
+            this.createUser.Click += new System.EventHandler(this.crearUser_Click);
+            // 
+            // login
+            // 
+            this.login.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(117)))), ((int)(((byte)(214)))));
+            this.login.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.login.ForeColor = System.Drawing.Color.White;
+            this.login.Location = new System.Drawing.Point(49, 318);
+            this.login.Name = "login";
+            this.login.Size = new System.Drawing.Size(175, 23);
+            this.login.TabIndex = 4;
+            this.login.Text = "Iniciar";
+            this.login.UseVisualStyleBackColor = false;
+            this.login.Click += new System.EventHandler(this.enviarEmail_Click);
+            // 
+            // passValue
+            // 
+            this.passValue.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.passValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passValue.Location = new System.Drawing.Point(23, 253);
+            this.passValue.Name = "passValue";
+            this.passValue.PasswordChar = '*';
+            this.passValue.Size = new System.Drawing.Size(241, 21);
+            this.passValue.TabIndex = 5;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(117)))), ((int)(((byte)(214)))));
+            this.panel2.Location = new System.Drawing.Point(23, 280);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(241, 2);
+            this.panel2.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(117)))), ((int)(((byte)(214)))));
+            this.label2.Location = new System.Drawing.Point(20, 144);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Usuario";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(117)))), ((int)(((byte)(214)))));
+            this.label3.Location = new System.Drawing.Point(20, 226);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Contraseña";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(292, 447);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.passValue);
+            this.Controls.Add(this.login);
+            this.Controls.Add(this.createUser);
+            this.Controls.Add(this.emailValue);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox emailValue;
+        private System.Windows.Forms.Button createUser;
+        private System.Windows.Forms.Button login;
+        private System.Windows.Forms.TextBox passValue;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ PassMan/HasherService.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassMan
+{
+    public class HasherService
+    {
+        private const int _saltSize = 16; // 128 bits
+        private const int _keySize = 20;
+        private const int _iterations = 10000;
+
+        //public static string HashPass(string password)
+        //{
+        //    SHA256 sha256 = SHA256.Create();
+
+        //    byte[] hashData = sha256.ComputeHash(Encoding.Default.GetBytes(password));
+        //    StringBuilder returnValue = new StringBuilder();
+
+        //    foreach (var hash in hashData)
+        //    {
+        //        returnValue.Append(hash.ToString());
+        //    }
+
+        //    return returnValue.ToString();
+        //}
+
+        public static string HashPass2(string password)
+        {
+            // Create salt
+            byte[] salt;
+            new RNGCryptoServiceProvider().GetBytes(salt = new byte[_saltSize]);
+
+            // Create Hash
+            var pbkdf2 = new Rfc2898DeriveBytes(password, salt, _iterations);
+            byte[] hash = pbkdf2.GetBytes(_keySize);
+
+            // Combine salt and Hash
+            byte[] hashBytes = new byte[_saltSize + _keySize];
+            Array.Copy(salt, 0, hashBytes, 0, _saltSize);
+            Array.Copy(hash, 0, hashBytes, _saltSize, _keySize);
+
+            // Convert to base64
+            var base64Hash = Convert.ToBase64String(hashBytes);
+            return base64Hash;
+        }
+
+        public static bool Verify(string password, string hashedPassword)
+        {
+            // Get hash bytes
+            var hashBytes = Convert.FromBase64String(hashedPassword);
+
+            // Get salt
+            var salt = new byte[_saltSize];
+            Array.Copy(hashBytes, 0, salt, 0, _saltSize);
+
+            // Create hash with given salt
+            var pbkdf2 = new Rfc2898DeriveBytes(password, salt, _iterations);
+            byte[] hash = pbkdf2.GetBytes(_keySize);
+
+            // Get result
+            for (var i = 0; i < _keySize; i++)
+            {
+                if (hashBytes[i + _saltSize] != hash[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
